bd60d6ac50d001c8044d7b5a06b7f56b020875d6
HoeunRyu <hoeun9565@gmail.com>
2025-02-03 19:14:34 +0900

    fix: ts module 세팅 수정, connectDB 삭제 및 npm run dev 테스트 완료

--- src/routes/company/controller.ts
+++ src/routes/company/controller.ts
@@ -1,13 +1,13 @@
 import express from "express";
-import service from "./service";
+import getCompanyList from "./getProductList";
 
 const router = express.Router();
 
 // 회사 관련 엔드포인트
-router.get("/", service.getBookmarks); // 회사 목록 조회
-router.get("/:id", service.getBookmarks); // 회사 상세 조회
-router.post("/", service.getBookmarks); // 회사 생성
-router.patch("/:id", service.getBookmarks); // 회사 정보 수정
-router.delete("/:id", service.getBookmarks); // 회사 삭제
+router.get("/", getCompanyList); // 회사 목록 조회
+// router.get("/:id", service.getBookmarks); // 회사 상세 조회
+// router.post("/", service.getBookmarks); // 회사 생성
+// router.patch("/:id", service.getBookmarks); // 회사 정보 수정
+// router.delete("/:id", service.getBookmarks); // 회사 삭제
 
 export default router;
