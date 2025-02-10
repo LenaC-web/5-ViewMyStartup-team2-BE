10c856c96c3b6a6ce9820c80d56d4571f5f50d5f
LenaC-web <lena.programming@gmail.com>
2025-02-10 23:36:45 +0900

    feat/comment API router 수정

--- src/routes/comment/controller.ts
+++ src/routes/comment/controller.ts
@@ -4,7 +4,7 @@ import service from "./service";
 const router = express.Router();
 
 // 코멘트 관련 엔드포인트
-router.get("/", service.getCompaniesCommentList); // 댓글 목록 조회
+router.get("/", service.getCompaniesCommentList); // 코멘트 목록 조회
 router.get("/:id", service.getCompaniesCommentListById); // 코멘트 목록 조회 by ID
 router.post("/", service.createCompaniesComment); //코멘트 생성
 router.patch("/:id", service.updateCompaniesComment); // 코멘트 수정
